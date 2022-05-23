0b4ffdbeca67525bfa57327c6aff1601a4c2e0d4
Alan Rojas <alan_rojas27@outlook.com>
2022-05-22 23:19:52 -0300

    Se agrego el navbar y el saludo

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = ()=>{
+    return(
+        <nav>
+            <button className="Button-Config">Inicio</button>
+            <button className="Button-Config">Mis Datos</button>
+            <button className="Button-Config">Mis Pedidos</button>
+            <button className="Button-Config">Descuentos</button>
+            <button className="Button-Config">Ayuda</button>
+        </nav>
+    )
+}
+
+export default NavBar
